f094d5d3c4056157d6325890580f7f0212e52acf
faisal-rafiq <faisalrafiq36666@gmail.com>
2021-09-25 21:06:42 +0500

    Update customJS.js

--- assets/js/customJS.js
+++ assets/js/customJS.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
     $('.landing-page').hide();
     $('.generate').hide();
-    $('.generate').hide();
-
+    $('.brief-result').show();
 
     $('#generate-link').on('click',function(){
         $('.landing-page').hide();
+        $('.brief-result').hide();
         $('.generate').show();
     });
 
+    $('#generate-link').on('click',function(){
+        $('.landing-page').hide();
+        $('.brief-result').hide();
+        $('.generate').show();
+    });
 
 });
